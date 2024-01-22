c6efbc313e9ce81249ab3829aafb2e8ab80a67d0
yscmark <markescarrilla@gmail.com>
2024-01-21 17:23:29 -0800

    Updated syntax, added comments

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+// Declares the member and the channel where the message is sent
 const memberID = '646341289792765953';
 const channelID = '1062973965036945479';
 
-client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
+// Mention user when a specific user joins a voice channel
+client.on(Events.VoiceStateUpdate, async (oldState) => {
     const user = await client.users.fetch(memberID);
     const channel = await client.channels.fetch(channelID);
-	if(oldState.member.id === memberID) {
+    if(oldState.member.id === memberID) {
         if(oldState.channelId === null) {
             console.log(user);
             channel.send('<@151797733508907008>');
